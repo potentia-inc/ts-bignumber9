1bb3b867a2a10aa56a588e40d74b73daa452adda
Tz-Huan Huang <tzhuan@potentia.asia>
2024-03-20 16:26:23 +0800

    Add jest matchers and bump release 1.1.0

--- src/jest.ts
+++ src/jest.ts
@@ -6,8 +6,11 @@ import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils'
 
 interface CustomMatchers<R = unknown> {
   toBeBigNumber: (this: unknown) => R
+  toBeBigNumberString: (this: unknown) => R
   toEqualBigNumber: (this: unknown, expected: unknown) => R
+  toEqualBigNumberString: (this: unknown, expected: unknown) => R
   toBeBigNumberNaN: (this: unknown) => R
+  toBeBigNumberNaNString: (this: unknown) => R
 }
 
 declare global {
@@ -40,6 +43,23 @@ export function toBeBigNumber(
   return { message, pass }
 }
 
+export function toBeBigNumberString(
+  this: unknown,
+  received: unknown,
+): jest.CustomMatcherResult {
+  const { isNot, promise } = this as unknown as This
+  const comment = 'BigNumber string type validity'
+  const options = { comment, isNot, promise }
+  const pass = typeof received === 'string' && !toBigNumber(received).isNaN()
+  const message = getMessage(
+    pass,
+    matcherHint('toBeBigNumberString', undefined, undefined, options),
+    printReceived(received),
+    printExpected('BigNumber string'),
+  )
+  return { message, pass }
+}
+
 export function toEqualBigNumber(
   this: unknown,
   received: unknown,
@@ -59,6 +79,25 @@ export function toEqualBigNumber(
   return { message, pass }
 }
 
+export function toEqualBigNumberString(
+  this: unknown,
+  received: unknown,
+  expected: unknown,
+): jest.CustomMatcherResult {
+  const { isNot, promise } = this as unknown as This
+  const comment = 'BigNumber string type and optional value equality'
+  const options = { comment, isNot, promise }
+  const pass =
+    typeof received === 'string' && toBigNumber(received).eq(toBigNumber(expected))
+  const message = getMessage(
+    pass,
+    matcherHint('toEqualBigNumberString', undefined, undefined, options),
+    printReceived(received),
+    printExpected(toBigNumber(expected).toFixed()),
+  )
+  return { message, pass }
+}
+
 export function toBeBigNumberNaN(
   this: unknown,
   received: unknown,
@@ -72,6 +111,23 @@ export function toBeBigNumberNaN(
     matcherHint('toBeBigNumberNaN', undefined, undefined, options),
     printReceived(received),
     printExpected(NaN),
+  )
+  return { message, pass }
+}
+
+export function toBeBigNumberNaNString(
+  this: unknown,
+  received: unknown,
+): jest.CustomMatcherResult {
+  const { isNot, promise } = this as unknown as This
+  const comment = 'BigNumber NaN string equality'
+  const options = { comment, isNot, promise }
+  const pass = received === 'NaN'
+  const message = getMessage(
+    pass,
+    matcherHint('toBeBigNumberNaNString', undefined, undefined, options),
+    printReceived(received),
+    printExpected('NaN'),
   )
   return { message, pass }
 }
